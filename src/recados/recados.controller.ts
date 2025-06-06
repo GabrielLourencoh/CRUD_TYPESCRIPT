9dcff3a8d9a2cf823cf8c2d6bed16c702c82cbcd
GabrielLourencoh <gl930551@gmail.com>
2025-06-06 17:58:21 -0300

    Configurando o eslint e o prettierrc, e utilizando-os no projeto

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,4 +1,17 @@
-import { Body, Patch, Query, Delete, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Body,
+  Patch,
+  Query,
+  Delete,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 //CRUD
 // Create -> POST -> Criar um recado
@@ -12,35 +25,38 @@ import { Body, Patch, Query, Delete, Controller, Get, HttpCode, HttpStatus, Para
 
 @Controller('recados') // Decorator de controle
 export class RecadosController {
-    // @HttpCode(201) com numero direto
-    @HttpCode(HttpStatus.OK) // Retorna 200
-    @Get('/') // Encontra todos os recados
-    findAll(@Query() pagination: any) {
-        const { limit = 10, offset = 0 } = pagination;
-        return `Retorna todos os recados. Limit=${limit}, Offset=${offset}`;
-    }
+  // @HttpCode(201) com numero direto
+  @HttpCode(HttpStatus.OK) // Retorna 200
+  @Get('/') // Encontra todos os recados
+  findAll(@Query() pagination: any) {
+    const { limit = 10, offset = 0 } = pagination;
+    return `Retorna todos os recados. Limit=${limit}, Offset=${offset}`;
+  }
 
-    // Encontra um recado
-    @Get(':id') //estrutura (:nomedoparametroquequeremos)
-    findOne(@Param('id') id: string) { // O decorator Param nos permite pegar determinados parametros da url
-        return `Essa roda retorna UM recado ID ${id}`; // Usando o `` no lugar da '', podemos colocar variaveis na nossa msg
-    }
+  // Encontra um recado
+  @Get(':id') //estrutura (:nomedoparametroquequeremos)
+  findOne(@Param('id') id: string) {
+    // O decorator Param nos permite pegar determinados parametros da url
+    return `Essa roda retorna UM recado ID ${id}`; // Usando o `` no lugar da '', podemos colocar variaveis na nossa msg
+  }
 
-    @Post()
-    create(@Body() body: any) { // Decorator @Body serve para trazer os dados enviados da nossa requisição e na frente, definimos a variavel que recebe esses dados enviados
-        return body;
-    }
+  @Post()
+  create(@Body() body: any) {
+    // Decorator @Body serve para trazer os dados enviados da nossa requisição e na frente, definimos a variavel que recebe esses dados enviados
+    return body;
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() body: any) { // Preciso do id e do body da requisição
-        return {
-            id, 
-            ...body
-        }
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    // Preciso do id e do body da requisição
+    return {
+      id,
+      ...body,
+    };
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) { 
-        return `Essa roda apaga o recado ID ${id}`;
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Essa roda apaga o recado ID ${id}`;
+  }
 }
