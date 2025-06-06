fff54d1cbd63650f0992861192e2ee6ea13519d0
GabrielLourencoh <gl930551@gmail.com>
2025-06-06 16:45:38 -0300

    Criando um metodo POST, aprendendo o decorator @Body, utilizando um JSON para enviar dados, fazendo um UPDATE com o metodo PATCH nos recados, utilizando tanto o parametro @Param e o @Body no processo de UPDATE de um recado

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Patch, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+
+//CRUD
+// Create -> POST -> Criar um recado
+// Read -> GET -> ler todos os recados
+// Read -> GET -> ler um recado
+// Update -> PACTH / PUT -> Atualizar um recado
+// Delete -> DELETE -> Apagar um recado
+
+// PATCH é utilizado para atualizar dados de um recurso
+// PUT é utilizado para atualizar um recurso inteiro
 
 @Controller('recados') // Decorator de controle
 export class RecadosController {
-    // Encontra todos os recados
-    @Get('/')
+    // @HttpCode(201) com numero direto
+    @HttpCode(HttpStatus.OK) // Retorna 200
+    @Get('/') // Encontra todos os recados
     findAll() {
         return 'Essa roda retorna todos os recados';
     }
@@ -13,5 +24,18 @@ export class RecadosController {
     findOne(@Param('id') id: string) { // O decorator Param nos permite pegar determinados parametros da url
         console.log(id);
         return `Essa roda retorna UM recado ID ${id}`; // Usando o `` no lugar da '', podemos colocar variaveis na nossa msg
+    }
+
+    @Post()
+    create(@Body() body: any) { // Decorator @Body serve para trazer os dados enviados da nossa requisição e na frente, definimos a variavel que recebe esses dados enviados
+        return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body: any) { // Preciso do id e do body da requisição
+        return {
+            id, 
+            ...body
+        }
     }
 }
