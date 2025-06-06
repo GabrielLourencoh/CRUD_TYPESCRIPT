c267776a34b29a859e1fc1085cc17dd1a69c1023
GabrielLourencoh <gl930551@gmail.com>
2025-06-06 19:02:17 -0300

    Criando uma entidade para o Recado, criando o service completo da nossa aplicação, ajustando o controller para receber esse service de maneira correta e tambem realizando os testes dentro da nossa API Client

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -33,33 +33,30 @@ export class RecadosController {
   @Get('/') // Encontra todos os recados
   findAll(@Query() pagination: any) {
     const { limit = 10, offset = 0 } = pagination;
-    return `Retorna todos os recados. Limit=${limit}, Offset=${offset}`;
+    return this.recadosService.findAll();
   }
 
   // Encontra um recado
   @Get(':id') //estrutura (:nomedoparametroquequeremos)
   findOne(@Param('id') id: string) {
     // O decorator Param nos permite pegar determinados parametros da url
-    return `Essa roda retorna UM recado ID ${id}`; // Usando o `` no lugar da '', podemos colocar variaveis na nossa msg
+    return this.recadosService.findOne(id); // Usando o `` no lugar da '', podemos colocar variaveis na nossa msg
   }
 
   @Post()
   create(@Body() body: any) {
     // Decorator @Body serve para trazer os dados enviados da nossa requisição e na frente, definimos a variavel que recebe esses dados enviados
-    return body;
+    return this.recadosService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body: any) {
     // Preciso do id e do body da requisição
-    return {
-      id,
-      ...body,
-    };
+    return this.recadosService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `Essa roda apaga o recado ID ${id}`;
+    return this.recadosService.remove(id);
   }
 }
--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -1,8 +1,74 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Injectable } from '@nestjs/common';
+import { Recado } from './entities/recado.entity';
 
 @Injectable()
 export class RecadosService {
-  hello() {
-    return 'Hello world!';
+  private lastId = 1; // Nosso ultimo recado
+  private recados: Recado[] = [
+    // Array que carrega nossos recados
+    {
+      id: 1,
+      texto: 'Este é um recado de teste',
+      de: 'Gabriel',
+      para: 'Neusa',
+      lido: false,
+      data: new Date(),
+    },
+  ];
+
+  findAll() {
+    return this.recados;
+  }
+
+  findOne(id: string) {
+    return this.recados.find(item => item.id === +id); // Ele acha o recado 1, e o '+' na frente do id, transforma ele de string para number
+  }
+
+  create(body: any) {
+    // Pega o body como parametro
+    this.lastId++; // Adiciona no ultimo id
+    const id = this.lastId; // PEga o ultimo id e atribui ao novo criado
+
+    const novoRecado = {
+      // Cria um novo recado com o id e o body passado
+      id,
+      ...body,
+    };
+    this.recados.push(novoRecado); // Adiciona o novo recado no array de recados
+
+    return novoRecado;
+  }
+
+  update(id: string, body: any) {
+    const recadoExistenteIndex = this.recados.findIndex(
+      // Pega o indice passado na url
+      item => item.id === +id,
+    );
+
+    if (recadoExistenteIndex >= 0) {
+      // Verifica se o indice é existente
+      const recadoExistente = this.recados[recadoExistenteIndex]; // Cria uma variavel que recebe os dados que tinha no array recados no indice passado
+
+      this.recados[recadoExistenteIndex] = {
+        // Inverte os dados que tinha naquela posição pra um novo passado
+        ...recadoExistente,
+        ...body,
+      };
+    }
+  }
+
+  remove(id: string) {
+    const recadoExistenteIndex = this.recados.findIndex(
+      // Achando o indice do recado que desejamos apagar
+      item => item.id === +id,
+    );
+
+    if (recadoExistenteIndex >= 0) {
+      // Se ele existe
+      this.recados.splice(recadoExistenteIndex, 1); // Nos apagamos o item, se nao existe, nao faz nada
+    }
   }
 }
