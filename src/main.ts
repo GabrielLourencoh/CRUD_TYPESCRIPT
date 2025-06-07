7574a596aae83239ed2902ad370f55b33b1f33d2
GabrielLourencoh <gl930551@gmail.com>
2025-06-06 21:36:45 -0300

    Adicionando o whitelist e forbidNonWhitelisted para validações de segurança

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Aqui, importamos o class-validator e o class-transformer, dessa forma: npm i class-validator class-transformer
-  app.useGlobalPipes(new ValidationPipe()); // Criando uma instancia do validationpipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove chaves que não estao no DTO
+      forbidNonWhitelisted: true, // Levantar erro quando a chave nao existir
+    }),
+  ); // Criando uma instancia do validationpipe
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
