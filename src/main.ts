5f374238772dfcc33c691b3822a29e4e936ef9e8
GabrielLourencoh <gl930551@gmail.com>
2025-06-06 21:04:37 -0300

    VALIDANDO DADOS DE ENTRADA COM useGlobalPipes, ValidationPipe e class-validador no create-recado.dto.ts e realizando testes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Aqui, importamos o class-validator e o class-transformer, dessa forma: npm i class-validator class-transformer
+  app.useGlobalPipes(new ValidationPipe()); // Criando uma instancia do validationpipe
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,6 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateRecadoDto {
+  // @isNumber()
+  // @isBoolean()
+  // @isDate()
+  //  @IsOptional
   // Uma boa pratica é deixar como readonly, para que não possa ser alterados
+  @IsString() // Esse texto precisa ser uma String
+  @IsNotEmpty() // Esse texto nao pode estar vazio
+  @MinLength(5) // Esse texto precisa ter mais que 5 caracteres
+  @MaxLength(255) // Esse texto tem o maximo de caracteres 255
   readonly texto: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly de: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly para: string;
 }
