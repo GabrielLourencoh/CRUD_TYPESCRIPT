6f545681d1f21b4c45169f489eedeb04369abb90
GabrielLourencoh <gl930551@gmail.com>
2025-06-07 12:02:35 -0300

    Criando a primeira entity do TypeORM (Primeira tabela no banco de dados PSQL), configurando o recados.module.ts para receber essa Feature através do import e testando no DBeaver se essa tabela foi criada

--- src/recados/entities/recado.entity.ts
+++ src/recados/entities/recado.entity.ts
@@ -1,8 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
 export class Recado {
-  id: number;
+  @PrimaryGeneratedColumn() // Coluna gerada como um id
+  id: number; // Poderia ser um UUID, o id deve ser sequencial: 1, 2, 3, 4, 5
+
+  @Column({ type: 'varchar', length: '255' }) // Se não passar nada, continua sendo uma coluna de texto sem tamanho mesmo
   texto: string;
+
+  @Column({ type: 'varchar', length: '50' })
   de: string;
+
+  @Column({ type: 'varchar', length: '50' })
   para: string;
+
+  @Column({ default: false }) // Valor padrão é falso
   lido: boolean;
-  data: Date;
+
+  @Column()
+  data: Date; // createdAt
+
+  @CreateDateColumn()
+  createdAt?: Date; // createdAt
+
+  @CreateDateColumn()
+  updateAt?: Date; // updateAt
 }
--- src/recados/recados.module.ts
+++ src/recados/recados.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RecadosController } from './recados.controller';
 import { RecadosService } from './recados.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { Recado } from './entities/recado.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Recado])],
   controllers: [RecadosController],
   providers: [RecadosService],
 })
