e6a3a2247e341ca140eef09021191b0eb11d4488
GabrielLourencoh <gl930551@gmail.com>
2025-06-07 11:19:47 -0300

    Instalando o postgreSQL e o Dbeaver, instalando o typeORM e o typeORM pg, dessa forma: 'npm i @nestjs/typeorm typeorm pg', e realizando as configurações do mesmo no meu modulo raiz (app.module.ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecadosModule } from 'src/recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432, // porta padrao
+      username: 'postgres', // username padrao
+      database: 'postgres', // database padrao
+      password: '240024', // senha do banco
+      autoLoadEntities: true, // Carrega entidades sem precisar especificá-las
+      synchronize: true, // Sincroniza com o BD. Não deve ser usado em produção
+    }),
+    RecadosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
