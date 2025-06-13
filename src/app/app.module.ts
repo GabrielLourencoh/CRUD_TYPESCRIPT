e4f26322a3d5d3e1093a4f176e2740f26486ff52
GabrielLourencoh <gl930551@gmail.com>
2025-06-13 11:41:27 -0300

    Trocando a conexao com o banco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { PessoasModule } from 'src/pessoas/pessoas.module';
       host: 'localhost',
       port: 5432, // porta padrao
       username: 'postgres', // username padrao
-      database: 'postgres', // database padrao
+      database: 'meu_crud_recados', // database padrao
       password: '240024', // senha do banco
       autoLoadEntities: true, // Carrega entidades sem precisar especificá-las
       synchronize: true, // Sincroniza com o BD. Não deve ser usado em produção
