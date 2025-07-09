59596ed430c51bb7fa0a50ccee7009171992426a
GabrielLourencoh <gl930551@gmail.com>
2025-07-09 14:39:19 -0300

    Autenticando o usuário com e-mail e senha usando o Hash de senha e HashingService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+    private readonly hashingService: HashingService,
+  ) {}
+
   async login(loginDto: LoginDto) {
-    return loginDto;
+    const pessoa = await this.pessoaRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    // Chega se a pessoa não existe
+    if (!pessoa) {
+      throw new UnauthorizedException('Pessoa não existe');
+    }
+
+    const passwordIsValid = await this.hashingService.compare(
+      loginDto.password,
+      pessoa.passwordHash,
+    );
+
+    // Checa se a senha não é valida
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Senha inválida');
+    }
+
+    // Aqui, criaremos um novo token (do JWT) e vamos entregar ao usuário na resposta
+
+    return {
+      message: 'Usuário logado!',
+    };
   }
 }
