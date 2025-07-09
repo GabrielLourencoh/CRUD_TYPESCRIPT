f2ae9bb672d0295e786d2b6867e40eb4e68c9487
GabrielLourencoh <gl930551@gmail.com>
2025-07-09 14:14:27 -0300

    Criando o AuthService e AuthController para rota de Login do usuário

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { Global, Module } from '@nestjs/common';
 import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Global() // Pode ser usado na aplicação inteira
 @Module({
+  controllers: [AuthController],
   providers: [
     {
       provide: HashingService, // Toda vez que for chamado esse contrato
       useClass: BcryptService, // Usa essa classe aqui que implementa o contrato HashingService
     },
+    AuthService,
   ],
   exports: [HashingService],
 })
