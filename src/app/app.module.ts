3c8342a02fa3e66d6d27a3d28023c4e435eaf361
GabrielLourencoh <gl930551@gmail.com>
2025-08-19 17:02:43 -0300

    Utilizando o ConfigService para obter valores do .env diretamente no módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { RecadosModule } from 'src/recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
 import { AuthModule } from 'src/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import jwtConfig from 'src/auth/config/jwt.config';
 import * as Joi from '@hapi/joi';
+import appConfig from './app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       // envFilePath: '.env', // Caminho da variavel de ambiente
       // ignoreEnvFile: true, // Ignora arquivos .env
+      load: [appConfig],
       validationSchema: Joi.object({
         DATABASE_TYPE: Joi.required(),
         DATABASE_HOST: Joi.required(),
@@ -25,15 +27,33 @@ import * as Joi from '@hapi/joi';
         DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0),
       }),
     }),
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT ?? '5432', 10), // porta padrao
-      username: process.env.DATABASE_USERNAME, // username padrao
-      database: process.env.DATABASE_DATABASE, // database padrao
-      password: process.env.DATABASE_PASSWORD, // senha do banco
-      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // Carrega entidades sem precisar especificá-las
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Sincroniza com o BD. Não deve ser usado em produção
+    // TypeOrmModule.forRoot({
+    //   type: process.env.DATABASE_TYPE as 'postgres',
+    //   host: process.env.DATABASE_HOST,
+    //   port: parseInt(process.env.DATABASE_PORT ?? '5432', 10), // porta padrao
+    //   username: process.env.DATABASE_USERNAME, // username padrao
+    //   database: process.env.DATABASE_DATABASE, // database padrao
+    //   password: process.env.DATABASE_PASSWORD, // senha do banco
+    //   autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // Carrega entidades sem precisar especificá-las
+    //   synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Sincroniza com o BD. Não deve ser usado em produção
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get<'postgres'>('database.type'),
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          username: configService.get<string>('database.username'),
+          database: configService.get<string>('database.database'),
+          password: configService.get<string>('database.password'),
+          autoLoadEntities: configService.get<boolean>(
+            'database.autoLoadEntities',
+          ),
+          synchronize: configService.get<boolean>('database.synchronize'),
+        };
+      },
     }),
     RecadosModule,
     PessoasModule,
